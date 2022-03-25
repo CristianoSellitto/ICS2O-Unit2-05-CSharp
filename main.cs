970ee25efee01bce558430c2667a199da63e6858
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-25 17:07:09 +0000

    Added program

--- main.cs
+++ main.cs
@@ -1,7 +1,20 @@
+// Cristiano
+// ICS2O-Unit2-05-CSharp
+// March 25 2022
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        float hours;
+        float hourlypay;
+        Console.WriteLine("Enter hours worked:");
+        hours = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter hourly pay:");
+        hourlypay = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("You'll earn $" + ((hours * hourlypay) * (1 - 0.18)).ToString("0.00") + ", and the government will earn $" + ((hours * hourlypay) * 0.18).ToString("0.00") + ".");
+        Console.WriteLine("\nFinished.");
+    }
 }
